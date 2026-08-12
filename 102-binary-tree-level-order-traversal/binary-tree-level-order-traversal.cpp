0fea133b6fbc7f09426cdb6a0862c61bdbb1d941
Abhishek <sauravk8825@gmail.com>
2026-08-12 20:02:34 +0530

    Time: 0 ms (100.00%) | Memory: 17.2 MB (16.31%) - LeetSync

--- 102-binary-tree-level-order-traversal/binary-tree-level-order-traversal.cpp
+++ 102-binary-tree-level-order-traversal/binary-tree-level-order-traversal.cpp
@@ -1,38 +1,30 @@
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
- */
+
 class Solution {
 public:
-    int levels(TreeNode *root){
-        if(root==NULL){
-            return 0;
+    void level(TreeNode* root,vector<vector<int>>&ans){
+        queue<TreeNode*>q;
+        if(root==NULL) return ;
+        q.push(root);
+        while(!q.empty()){
+            vector<int>level;
+            int size=q.size();
+            for(int i=0;i<size;i++){
+            TreeNode* temp=q.front();
+            q.pop();
+            level.push_back(temp->val);
+            if(temp->left!=NULL){
+                q.push(temp->left);
+            }
+            if(temp->right!=NULL){
+                q.push(temp->right);
+            }
         }
-        return 1+max(levels(root->left),levels(root->right));
+        ans.push_back(level);
+        }
     }
-    void lOrder(TreeNode* root,vector<vector<int>>&ans,int level){
-        if(root==NULL){
-            return; }
-            ans[level].push_back(root->val);
-    lOrder(root->left,ans,level+1);
-    lOrder(root->right,ans,level+1);
-    }
-    
     vector<vector<int>> levelOrder(TreeNode* root) {
-        int n=levels(root);
-        vector<vector<int>>ans(n);
-        // for(int i=1;i<n;i++){
-        //     vector<int>v;
-        //     ans.push_back(v);
-        // }
-        lOrder(root,ans,0);
+        vector<vector<int>>ans;
+        level(root,ans);
         return ans;
     }
 };
