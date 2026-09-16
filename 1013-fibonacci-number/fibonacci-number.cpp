a4300adf15e2bddd32f0eff0d4747f3ab70a9a93
Abhishek <sauravk8825@gmail.com>
2026-09-16 11:52:57 +0530

    Time: 0 ms (100.00%) | Memory: 8.2 MB (11.28%) - LeetSync

--- 1013-fibonacci-number/fibonacci-number.cpp
+++ 1013-fibonacci-number/fibonacci-number.cpp
@@ -11,10 +11,22 @@ public:
          if(n==0 || n==1){
             return n;
         }
+        // if ans already exist return ans
         if(dp[n]!=-1){
             return dp[n];
         }
+        // store dp array and return 
         dp[n]=solveMem(n-1,dp)+solveMem(n-2,dp);
+        return dp[n];
+    }
+    int solveTab(int n){
+        // create a dp array
+        vector<int>dp(n+2,0);
+        dp[0]=0;
+        dp[1]=1;
+        for(int i=2;i<=n;i++){
+            dp[i]=dp[i-1]+dp[i-2];
+        }
         return dp[n];
     }
     int fib(int n) {
@@ -22,7 +34,11 @@ public:
     // return solveRec(n); 
     //solved using dp
     //method1 memoization
-    vector<int>dp(n+1,-1);
-    return solveMem(n,dp);
+    // step-1-> find type of dp
+    // step-2-> crete a dp array and pass dp array
+    // vector<int>dp(n+1,-1);
+    // return solveMem(n,dp);
+    //Method-2->Tabulation
+    return solveTab(n);
     }
 };
