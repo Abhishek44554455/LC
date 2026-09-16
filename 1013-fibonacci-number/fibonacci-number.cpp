22e373ba701f1c0140c3e3b7ab8022f5ef604547
Abhishek <sauravk8825@gmail.com>
2026-09-16 11:12:07 +0530

    Time: 11 ms (37.02%) | Memory: 8.1 MB (17.08%) - LeetSync

--- 1013-fibonacci-number/fibonacci-number.cpp
+++ 1013-fibonacci-number/fibonacci-number.cpp
@@ -7,7 +7,22 @@ public:
         int ans=solveRec(n-1)+solveRec(n-2);
         return ans;
     }
+    int solveMem(int n,vector<int>&dp){
+         if(n==0 || n==1){
+            return n;
+        }
+        if(dp[n]!=-1){
+            return dp[n];
+        }
+        dp[n]=solveRec(n-1)+solveRec(n-2);
+        return dp[n];
+    }
     int fib(int n) {
-    return solveRec(n);   
+    
+    // return solveRec(n); 
+    //solved using dp
+    //method1 memoization
+    vector<int>dp(n+1,-1);
+    return solveMem(n,dp);
     }
 };
