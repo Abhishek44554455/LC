5f0967bade7d9343f023881fd43708d4c578147d
Abhishek <sauravk8825@gmail.com>
2026-09-19 12:23:30 +0530

    Time: 0 ms (100.00%) | Memory: 27.3 MB (25.84%) - LeetSync

--- 42-trapping-rain-water/trapping-rain-water.cpp
+++ 42-trapping-rain-water/trapping-rain-water.cpp
@@ -1,30 +1,23 @@
 class Solution {
 public:
-vector<int>getLeftMax(vector<int>arr,int n){
-    vector<int>leftMax(n);
-     leftMax[0]=arr[0];
-    for(int i=1;i<n;i++){
-        leftMax[i]=max(leftMax[i-1],arr[i]);
-    }
-    return leftMax;
-}
-vector<int>getRightMax(vector<int>arr,int n){
-    vector<int>rightMax(n);
-     rightMax[n-1]=arr[n-1];
-    for(int i=n-2;i>=0;i--){
-        rightMax[i]=max(rightMax[i+1],arr[i]);
-    }
-    return rightMax;
-}
-    int trap(vector<int>& height) {
-        int n=height.size();
-        vector<int>leftMax=getLeftMax(height,n);
-        vector<int>rightMax=getRightMax(height,n);
-        int sum=0;
-        for(int i=1;i<n-1;i++){
-            int h=min(leftMax[i],rightMax[i])-height[i];
-            sum+=h;
+    int trap(vector<int>& arr) {
+        int res=0;
+        int n=arr.size();
+        vector<int>lmax(n);
+        vector<int>rmax(n);
+        lmax[0]=arr[0];
+        for(int i=1;i<n;i++){
+            lmax[i]=max(arr[i],lmax[i-1]);
         }
-        return sum;
+        rmax[n-1]=arr[n-1];
+        for(int i=n-2;i>=0;i--){
+            rmax[i]=max(arr[i],rmax[i+1]);
+        }
+        for(int i=1;i<n;i++){
+            res=res+(min(lmax[i],rmax[i])-arr[i]);
+        }
+
+        return res;
+
     }
 };
